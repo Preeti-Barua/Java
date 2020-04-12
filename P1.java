5de5570638dcdecadddf112c52e0caefd2dcf245
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2020-04-12 23:03:02 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,18 @@
+
+
+interface P1{  
+void print();  
+}  
+interface Showable{  
+void show();  
+}  
+class A7 implements P1,Showable{  
+public void print(){System.out.println("Hello");}  
+public void show(){System.out.println("Welcome");}  
+  
+public static void main(String args[]){  
+A7 obj = new A7();  
+obj.print();  
+obj.show();  
+ }  
+}  
