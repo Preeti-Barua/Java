1a3dd36530f29fc0ac520eea51f65f52ef3ead9e
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2020-06-28 18:46:48 +0530

    charAt

---
+++ String101.java
@@ -0,0 +1,15 @@
+public class String101 {
+    public static void main(String args[])
+    {
+        String A = "CARPE DIEM";
+                  //0123456789                 indexing
+
+            char let1= A.charAt(1);
+            char let2= A.charAt(3);
+
+
+            System.out.println(let1 +let2);         //here it will give the addition the ascii value of A n P.
+            System.out.println(let1+" " +let2); 
+    }
+    
+}
