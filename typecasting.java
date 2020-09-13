cb92158724fc1b82497cb2c62b15416113b8ede8
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2020-09-13 20:39:43 +0530

    typecasting
    narrowing n widening

---
+++ typecasting.java
@@ -0,0 +1,32 @@
+public class typecasting{
+    public static void main(String args[])
+    {
+
+        //widening typecasting
+        byte x=4;
+       int y=x;
+
+        int a=7;
+        double b=a;
+
+
+
+
+
+
+
+        System.out.println(b);
+        System.out.println(a);
+
+
+      //narrowing typecasting
+      double mydouble= 1.461;
+      int myInt= (int)mydouble;           
+
+      System.out.println(mydouble);
+      System.out.println(myInt);
+
+
+
+    }
+}
