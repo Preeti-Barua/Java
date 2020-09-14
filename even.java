b409e3367094a52450b3d0e2a329786bda7f921a
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2020-09-14 20:56:59 +0530

    checking number is even or odd
    hardcoded value

---
+++ even.java
@@ -0,0 +1,16 @@
+public class even{
+    public static void main(String args[])
+    {
+
+           int i=4;
+           if(i%2==0)
+           {
+               System.out.println("even");
+
+           }
+           else{
+               System.out.println("odd");
+           }
+    }
+    
+}
