7eae7f2d1ee7202248e6cd18c5bde8a4c2d605fd
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2020-09-14 20:57:37 +0530

    factorial of a number
    hardcoded value

---
+++ fact.java
@@ -0,0 +1,14 @@
+public class fact {
+    public static void main(String args[])
+    {
+
+        int n=5,i,f=1;
+        for(i=n;i>0;i--)
+        {
+            f=f*i;
+        }
+        System.out.println(f);
+        
+    }
+    
+}
