0c95bfce6091741ef059fe0a3356a95226721d66
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2020-09-21 23:38:20 +0530

    super keyword
    We can use super keyword to access the data member or field of parent class. It is used if parent class and child class have same fields.

---
+++ Superkeyword1.java
@@ -0,0 +1,22 @@
+class Animal{
+
+   String color ="white";
+}
+class Dog extends Animal{
+
+     String color ="black";
+     void printcolor()
+     {
+         System.out.println(color);
+         System.out.println(super.color);
+
+     }
+}
+public class Superkeyword1 {
+    public static void main(String args[])
+    {
+        Dog d= new Dog();
+        d.printcolor();
+    }
+    
+}
