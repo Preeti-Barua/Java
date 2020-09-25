09be781334a7c30b36aa4cc8a6a28ec301eacbd9
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2020-09-25 14:03:53 +0530

    adding example
    class which contains two methods with the same name but different arguments

---
+++ polymorphism/VB.java
@@ -0,0 +1,31 @@
+ 
+    class Simple{
+    
+    
+        void add(int a,int b)
+        {
+            System.out.println("sum of two:" + (a+b));
+    
+        }
+        void add(int a, int b, int c)
+        {
+            System.out.println("sum of three:" + (a+b+c));
+        }
+    
+    }
+    class VB{
+    
+    public static void main(String args[])
+    {
+    
+    Simple s= new Simple();
+    s.add(61,14);
+    s.add(5,20,2);
+    
+    
+    }
+}
+        
+    
+    
+
