699f1eb34baaa04bf224c831233c0a2f3115ef30
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2020-10-26 13:57:56 +0530

    Overloading Practice

---
+++ Polymorphism/Overidingpractice.java
@@ -0,0 +1,57 @@
+package overiding;
+class Shape{
+	
+	   void area()
+	   {
+		    System.out.println("calculating Area");
+	   }
+	   
+	   
+}
+class Circle extends Shape{
+	
+	void area(int r1)
+	{
+	    float pi=3.14f;
+	    int r=2;
+	  
+	    float areaofcircle= pi *r*r;
+	    System.out.println(areaofcircle);
+		
+	}
+	
+}
+class Triangle extends Shape
+{
+	void area()
+	{
+		int b=2;
+		int h=4;
+		 float a= b*h;
+		 float areaoftriangle=a/2;
+		 
+		 System.out.println(areaoftriangle);
+	}
+}
+
+
+class Rectangle extends Shape
+{
+	void area()
+	{
+		int l=2,b=3;
+		int areaofrect= l*b;
+		System.out.println(areaofrect);
+	}
+}
+public class Overidingpractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+            Rectangle r= new Rectangle();
+            r.area();
+            
+	}
+
+}
